cd6e00a3c80deba4d7ce5eecfb709a688670cb01
Lucas Madranges <lucas.madranges.pro@icloud.com>
2023-11-04 14:25:47 +0100

    1.0.7 : Script of the tabbed component added

--- script.js
+++ script.js
@@ -79,3 +79,25 @@ document.querySelector('.nav__links').addEventListener("click", (event) => {
         })
     }
 })
+
+///////////////////////////////////////
+// Tabbed component
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener("click", (event) => {
+    const clicked = event.target.closest('.operations__tab');
+
+    // Guard clause
+    if (!clicked) return
+
+    // Active tab
+    tabs.forEach((element) => element.classList.remove('operations__tab--active'))
+    clicked.classList.add('operations__tab--active');
+
+    // Activate content area
+    tabsContent.forEach((element) => element.classList.remove('operations__content--active'))
+    document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+})
